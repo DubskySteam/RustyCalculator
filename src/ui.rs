69de1ef2049d2abfa9ea83092f35cf05d98572bf
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-10-19 04:13:47 +0200

    provisional calculator ui to test functionality

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,39 @@
-use dioxus::prelude::*;
+use dioxus::{prelude::*, html::{body, button}};
 
 pub fn create_ui(cx: Scope) -> Element {
     cx.render(
         rsx! {
+            style { include_str!("css/main.css") }
             div {
-                "HI"
+                calculator(cx)
             }
         }
-             )
+        )
+}
+
+fn calculator(cx: Scope) -> Element {
+    cx.render(
+        rsx! {
+            body {
+                div {class:"buttons",
+                button {"/"}
+                button {"7"}
+                button {"8"}
+                button {"9"}
+                button {"*"}
+                button {"4"}
+                button {"5"}
+                button {"6"}
+                button {"-"}
+                button {"1"}
+                button {"2"}
+                button {"3"}
+                button {"+"}
+                button {"0"}
+                button {"."}
+                button {"="}
+                }
+            }
+        }
+    )
 }
