4fad970c3dbdc94a0f915bbe914676b27c8f309a
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-10-21 01:58:26 +0200

    re-factored logic to use string<>f64 conversions

--- src/functionality.rs
+++ src/functionality.rs
@@ -1,10 +1,25 @@
-pub fn eval_math(sum: &usize, num: usize, op: &usize) -> usize {
+// pub fn eval_math(sum: &f64, num: f64, op: &usize) -> f64 {
+//     match op {
+//         0 => num,
+//         1 => sum/num,
+//         2 => sum * num,
+//         3 => sum - num,
+//         4 => sum + num,
+//         _ => 0.0
+//     }
+// }
+
+//pub fn 
+
+pub fn eval_math(sum: &f64, num: &str, op: &usize) -> f64 {
+    println!("Calculating {} {} {}", sum, op, num);
+    let num = num.parse::<f64>().unwrap();
     match op {
         0 => num,
-        1 => sum/num,
-        2 => sum * num,
-        3 => sum + num,
-        4 => sum - num,
-        _ => 0
+        1 => *sum as f64/num,
+        2 => *sum as f64 * num,
+        3 => *sum as f64 - num,
+        4 => *sum as f64 + num,
+        _ => 0.0
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -13,7 +13,8 @@ pub fn create_ui(cx: Scope) -> Element {
 }
 
 fn calculator(cx: Scope) -> Element {
-    let sum = use_state(cx, || 0);
+    let sum = use_state(cx, || 0.0);
+    let num = use_ref(cx, || String::new());
     let op = use_state(cx, || 0);
     // Division = 1
     // Multiply = 2
@@ -23,38 +24,90 @@ fn calculator(cx: Scope) -> Element {
         rsx! {
             body {
                 h2 {"{sum}"}
+                h2 {"{num.read()}"}
                 div {
-                    class:"buttons",
+                    class: "buttons",
                     button {
                         onclick: move |_| op.set(1),
                         "/"}
                     button {
                         onclick: move |_| {
-                            sum.set(eval_math(sum.get(), 7, op.get()))
+                            num.with_mut(|x| x.push('7'));
                         },
                         "7"}
-                    button {"8"}
-                    button {"9"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('8'));
+                        },
+                        "8"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('9'));
+                        },
+                        "9"}
                     button {
                         onclick: move |_| op.set(2),
                         "*"}
-                    button {"4"}
-                    button {"5"}
-                    button {"6"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('4'));
+                        },
+                        "4"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('5'));
+                        },
+                        "5"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('6'));
+                        },
+                        "6"}
                     button {
                         onclick: move |_| op.set(3),
                         "-"}
-                    button {"1"}
-                    button {"2"}
-                    button {"3"}
                     button {
-                        onclick: move |_| op.set(4),
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('1'));
+                        },
+                        "1"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('2'));
+                        },
+                        "2"}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('3'));
+                        },
+                        "3"}
+                    button {
+                        onclick: move |_| {
+                            if num.read().len() > 0 {
+                                sum.set(num.read().parse::<f64>().unwrap());
+                                num.with_mut(|x| x.clear());
+                                //println!("num: {}", num.read());
+                            }
+                            op.set(4);
+                        },
                         "+"}
-                    button {"0"}
-                    button {"."}
-                    button {"="}
+                    button {
+                        onclick: move |_| {
+                            num.with_mut(|x| x.push('0'));
+                        },
+                        "0"}
+                    button {
+                        onclick: move |_| {
+                            //sum.set(eval_math(sum.get(), '.', op.get()))
+                        },
+                        "."}
+                    button {
+                        onclick: move |_| {
+                            sum.set(eval_math(sum.get(), &num.read(), op.get()));
+                            num.with_mut(|x| x.clear());
+                        },
+                        "="}
                 }
             }
-        }
-    )
+        }    )
 }
